1ff3687afddfffc6b7b9a2e08599235e8d7d0a15
Rathod <835514@cognizant.com>
2020-01-02 14:58:43 +0530

    Java

---
+++ src/com/cognizant/truyum/model/MenuItem.java
@@ -0,0 +1,15 @@
+package com.cognizant.truyum.model;
+
+import java.sql.Date;
+
+public class MenuItem {
+    private long id;
+    private String name;
+private float price;
+private boolean active;
+private Date dateOfLaunch;
+private String categery;
+private boolean freeDelivery;
+
+
+}
